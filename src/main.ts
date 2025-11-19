86636985e028ede4d0a7f15c63a360df6286fc64
EduardKhismatov <E.Khismatov@yandex.ru>
2025-11-19 16:32:46 +0600

    optimization

--- src/main.ts
+++ src/main.ts
@@ -22,32 +22,3 @@
 // const start = new Date().getTime();
 // console.log(fun(array, array2));
 // console.log(`Конец V1. Ушло ${new Date().getTime() - start} ms\n\n\n`);
-
-/*
-Напишите 2 функции для нахождения TLD (Top Level Domain) для переданной в функцию ссылки. Решение должно содержать минимум 2 различные реализации.
-
-НЕТ! Напишите 1 функцию для извлечения TLD. Вторую не нужно!
-
-site.com => com
-my.domain.ru => ru
-https://sub-domain.shop.lite => lite
-https://news.ru/path-to-page?query=true => ru
-*/
-
-// const extractionDomen = (urlString: string) => {
-//   const url = new URL(urlString);
-//   const hostMain = url.hostname.split('.');
-//   return hostMain.length > 1 ? hostMain[hostMain.length - 1] : hostMain[0];
-// };
-//
-// // Здесь TLD - ru
-// const sampleUri1 = 'https://username:password@www.example.ru:888/path.to/something?a=b&c=d.e.f.g';
-// console.log(extractionDomen(sampleUri1));
-//
-// // Здесь TLD - com
-// const sampleUri2 = 'https://www.example.com/path.to/something?a=b&c=d.e.f.g';
-// console.log(extractionDomen(sampleUri2));
-//
-// // Здесь TLD - localhost
-// const sampleUri3 = 'https://localhost/path.to/something?a=b&c=d.e.f.g';
-// console.log(extractionDomen(sampleUri3));
---
+++ src/optimization.ts
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker';
+
+const url = 'http://backend-mentor.tech/very-long-path';
+const data: Record<string, string> = {};
+let size = 1;
+
+const creatShots = (usr: string): string => {
+  let id: string = faker.string.nanoid(size);
+  let counter = 0;
+  while (id in data) {
+    id = faker.string.nanoid(size);
+    counter++;
+  }
+  if (counter > 3) {
+    size++;
+    console.log(`Понадобилось ${counter} повторных попыток, новый раз размер ${size}`);
+  }
+  data[id] = id;
+  return id;
+};
+
+for (let i = 0; i < 10_000_000; i++) {
+  creatShots(url);
+}
+const s = creatShots(url);
+console.log(s);
+// для 10_000_000 я так и не дождался
